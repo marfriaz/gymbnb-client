caf9ac54de3c51f8e815794ad6dc12ac9a42aef8
Marco Friaz <marfriaz@gmail.com>
2021-03-30 21:29:16 -0700

    updated api endpoint

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: "http://localhost:8000/api",
-  // API_ENDPOINT: "https://arcane-hollows-74456.herokuapp.com/api",
+  // API_ENDPOINT: "http://localhost:8000/api",
+  API_ENDPOINT: "https://arcane-hollows-74456.herokuapp.com/api",
   TOKEN_KEY: "gymbnb-client-auth-token",
 };
